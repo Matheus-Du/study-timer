c240d1bd9024770f2d03a7da22233474c526ac4c
Matheus-Du <duncanmatheus905@gmail.com>
2022-07-16 23:30:20 -0400

    implemented proper points counter.

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,6 +4,7 @@ import '../css/App.css';
 function App() {
 
   const [time, setTime] = useState(0);
+  const [points, setPoints] = useState(0);
   const [isRunning, setRunning] = useState(false);
 
   function resetTimer() {
@@ -18,18 +19,24 @@ function App() {
     var interval = null;
     if(isRunning) {
       interval = setInterval(() => {
+        // TODO: updates points every 1:01, so looks kinda weird & could have issues in future
+        if(time%60000 === 0 && time !== 0) {
+          setPoints(points => points + 1);
+        }
         setTime(time => time + 1000);
       }, 1000);
-    } else if(!isRunning && time != 0) {
+    } else if(!isRunning && time !== 0) {
       clearInterval(interval);
     }
-    return () => clearInterval(interval);
-  }, [isRunning, setRunning]);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isRunning, setRunning, points, setPoints, time, setTime]);
 
   return (
     <div className='App-header'>
       <div className='navBar'>
-        <p>Points: {Math.floor(time/60000)}</p>
+        <p>Points: {points}</p>
       </div>
       <div className='header'>
         <h2>Timer App</h2>
