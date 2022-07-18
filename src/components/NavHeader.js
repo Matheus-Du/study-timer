6fde44f101c244536e3e605f2793ce1cb1d0f62e
Matheus-Du <duncanmatheus905@gmail.com>
2022-07-17 21:07:25 -0400

    changed css on navbar to make points counter nicer

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -7,7 +7,7 @@ function NavHeader(props) {
 
     return (
         <Navbar sticky='top' className='navbar'>
-            <Navbar.Brand href="#home" className='name'>
+            <Navbar.Brand href="#home" id='brand'>
                 <img
                     src={logo}
                     className="logo"
@@ -18,10 +18,10 @@ function NavHeader(props) {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ml-auto">
+                <Nav.Item className='nav-link' id='point-counter'>Points: {props.currentPoints}</Nav.Item>
                 <Nav.Link href="#home" className='nav-link'>Home</Nav.Link>
                 <Nav.Link href="#rewards" className='nav-link'>Rewards</Nav.Link>
                 <Nav.Link href="#about" className='nav-link'>About</Nav.Link>
-                <Nav.Item className='nav-link'>Points: {props.currentPoints}</Nav.Item>
               </Nav>
             </Navbar.Collapse>
         </Navbar>
